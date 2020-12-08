7d7b6d3d566981cf1acb439bc854a77554ef4e17
ridbrain <ridbrain@gmail.com>
2020-12-08 23:03:45 +0300

    Initial Commit

---
+++ egorka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  egorka
+//
+//  Created by Виталий Яковлев on 08.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
